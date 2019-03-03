04bfa4e3098d7a9cdcaa696eb5351c0ae9de9d2d
Chandler Mathews <35784996+ctmathews2@users.noreply.github.com>
2019-03-03 12:14:02 -0800

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -5,23 +5,23 @@ public class MergeSort implements SortingAlgorithm{
 
 	public void sort(int a[]) 
 	{ 
-		if (a.length > 1) 
+		if (a.length > 1) //Base case
 		{ 
-			int[] left = getLeft(a);
+			int[] left = getLeft(a); //Seperate array into left and right sides
 			int[] right = getRight(a);
-			sort(left);
+			sort(left); //Sort each half
 			sort(right);
-			merge(left,right,a);
+			merge(left,right,a); //Merge halves on the way back up
 		} 
 	}
 
 	public void merge(int[] left, int[] right, int[] a){
-		int aIndex = 0;
+		int aIndex = 0; //Starting index of all arrays
 		int leftIndex = 0;
 		int rightIndex = 0;
 
-		while(leftIndex < left.length && rightIndex < right.length ){
-			if(left[leftIndex] <= right[rightIndex]){
+		while(leftIndex < left.length && rightIndex < right.length ){ //Repeat until one array is completed
+			if(left[leftIndex] <= right[rightIndex]){ //If left is bigger than right, add to new arrray and increase coresponding left or right index
 				a[aIndex] = left[leftIndex];
 				leftIndex++;
 			}
@@ -29,16 +29,16 @@ public class MergeSort implements SortingAlgorithm{
 				a[aIndex] = right[rightIndex];
 				rightIndex++;
 			}
-			aIndex++;			
+			aIndex++; //increase new array index			
 		}
 
-		while(leftIndex < left.length){
+		while(leftIndex < left.length){ //If any of the left array exists, add the rest
 			a[aIndex] = left[leftIndex];
 			leftIndex++;
 			aIndex++;
 		}
 
-		while(rightIndex < right.length){
+		while(rightIndex < right.length){ //If any of the right array exists, add the rest
 			a[aIndex] = right[rightIndex];
 			rightIndex++;
 			aIndex++;
@@ -46,12 +46,12 @@ public class MergeSort implements SortingAlgorithm{
 
 	}
 
-	public int[] getLeft(int[] a){
+	public int[] getLeft(int[] a){ //Get left half of array
 		int[] left = Arrays.copyOfRange(a, 0, a.length/2);
 		return left;
 	}
 
-	public int[] getRight(int[] a){
+	public int[] getRight(int[] a){ //Get right half of array
 		int[] right = Arrays.copyOfRange(a, (a.length/2), a.length );
 		return right;
 	}
